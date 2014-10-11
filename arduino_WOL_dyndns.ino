6f69fa3e51d55cccb7d97f0fdc8e33aa4d3514e4
benoit Fauque <benoit.fauque@gmail.com>
2014-10-11 22:52:01 +0200

    remove the DDNS part, and add TCP Debug

--- arduino_WOL_dyndns.ino
+++ arduino_WOL_dyndns.ino
@@ -48,77 +48,94 @@
   #define DEBUG(x)
 #endif
 
-
-unsigned int timer =0;
-byte arduinoMAC[] = {ADDRESSE_MAC_ARDUINO}; 				// Adresse MAC Arduino sticker
-IPAddress ipLocal(IP_LOCAL_ARDUINO);							// Adresse IP de l'arduino 
-IPAddress broadcastIP(IP_BROADCAST_ARDUINO);					// Adresse IP de Broadcast LAN
+/*variable global*/
 EthernetUDP udp; 											// Création d'Un objet de classe EthernetUDP
 EthernetServer tcpServer(PORT_EXTINCTION_SERVER);
-static char * IPPublic = NULL;
+String last_debug_msg; 
+
 void setup() {
+	byte arduinoMAC[] = {ADDRESSE_MAC_ARDUINO}; 				// Adresse MAC de la Arduino
+    IPAddress ipLocal(IP_LOCAL_ARDUINO);							// Adresse IP de la Arduino 
 	pinMode(PINBUTTON1, INPUT);
 	pinMode(PINBUTTON2, INPUT);
-  	IPPublic= (char*) calloc(sizeof(char), 15); // Adresse IP public actuelle. (taille max : 15. min : 11)
-        if(IPPublic == NULL)
-          DEBUGLN(("Allocation de IPPublic impossible"));
+ 
 	delay(1000);
 	Serial.begin(9600); // set up Serial library at 9600 bps
 	DEBUGLN(("c'est parti !"));
 	Ethernet.begin(arduinoMAC,ipLocal);
-	DEBUGLN(("Ethernet connecter !"));
+	DEBUGLN(("TCP connecter !"));
 	udp.begin(PORT_DISTANT_WOL);
 	tcpServer.begin();
 	DEBUGLN(("On passe a la boucle principale \n"));
 }
 
 void loop() {
+    static unsigned int timer =0;
 
-	if(timer == 72000){ // On s'occupe des DNS toutes les 2h
-		findAndSetIPPublic(IPPublic);
-		timer = 0;
-	}
-	if(timer % 10 == 0){// On s'occupe du wol reseaux  toutes les 1 seconde
+	if(timer % 50 == 0){			// On s'occupe du wol reseaux  toutes les 1 secondes
 		check_wol_magic_packet();
+		timer = 0;
 	}
 	check_button(PINBUTTON1);
 	check_button(PINBUTTON2);
-	check_extinction();
-	delay(100);
+	check_ethernet_msg();
+	delay(20);
 	timer++;
 }
 /**
 * \brief vérifie une connection réseau, et regarde si elle correspond à l'extinction de l'écran.
 */
-void check_extinction(void){
+void check_ethernet_msg(void){
 	EthernetClient client = tcpServer.available();
+	String stringstr;
+
   	if (client) {
   		DEBUGLN(("y a un client !"));
   		char c;
-  		char * str = (char* )calloc(sizeof(char),12);
-                if(str == NULL)
-                  DEBUGLN(("Allocation de str dans check_extinction impossible"));
-		char * strP = str; // pointeur sur la chaine d'entrée str
-
-    	while (client.connected()) {
-      		if (client.available()) {
+    	if (client.connected()) {
+      		while (client.available()) {
 
        			c = client.read();
-          		*strP = c;
-				strP++;
+          		stringstr += c;
+				DEBUGLN((stringstr));
         	} 
 		}
-		DEBUG(("msg recu : "));
-		DEBUGLN((str));
-		if(strcmp(str, "extinction")==0){
-			delay(5000); //on attend pour verifier la bonne extinction de windows
-			Remote_OFF();
-			Remote_OFF();
-		}
-		free(str);
+		stringstr.trim();
+		DEBUG(("msg recu :_"));
+		DEBUGLN((stringstr));
+		test_msg(stringstr, &client);
+		//client.stop();
 	}
 }
 
+void test_msg(String str,EthernetClient *client){
+	char otherString[50];
+	unsigned long time;
+	time = millis();
+
+	DEBUG(("msg dans test_msg : "));
+	DEBUGLN((str));
+
+	if( str == "extinction\n"){
+		client->write("ok pour l'extinction\n");
+		delay(5000); //on attend pour verifier la bonne extinction de windows
+		Remote_OFF();
+		Remote_OFF();
+		last_debug_msg = "Extinction ecran";
+
+	}
+	if( str == "debug" || str == "debug\n"){
+		last_debug_msg+="\nuptime :";
+		last_debug_msg+=time;
+
+		last_debug_msg.toCharArray(otherString, last_debug_msg.length() + 1); 
+		client->write(otherString);
+
+		DEBUGLN(("msg de debug sans RaL "));
+		DEBUGLN((last_debug_msg));
+		DEBUGLN((otherString));
+	}
+}
 /**
  * \brief Si un bouton est appuyé, on envoie un paquet magique.
  * \param inPin Pin du bouton.
@@ -137,144 +154,23 @@ void check_button(int inPin){
 		DEBUGLN((" appuyer"));
 		wol_send_packet(NULL);  // Allume le PC
 		DEBUGLN(("fin check bouton"));
+		last_debug_msg = "Bouton appuyé : ";
+		last_debug_msg += inPin ;
+
 	} 
 }
 
-/**
- * \brief met a jour l'adresse IP public si elle a changé depuis la dernière vérif.
- */
-void findAndSetIPPublic(char * IPPublic){
-		static char * IPPublicNew;
 
-		IPPublicNew = IpAdressPublic();
-		DEBUG(("\nIPPublicNew :"));
-		DEBUGLN((IPPublicNew));
-		DEBUG(("IPPublic :"));
-		DEBUGLN((IPPublic));
-		if(strcmp(IPPublicNew, IPPublic) != 0)
-		{
-			   setIpAdressPublic(IPPublicNew);
-		}
-		free(IPPublic);
-		IPPublic = IPPublicNew;
-}
-
-/**
- * \brief Transmet une adresse IP au service DynDns en fonction de l'host.
- *
- * \param IPpublic nouvelle IP a joindre a l'host.
- * \return retourne 1 si tout va bien, et 0 en cas d'erreur de connection.
- */
-int setIpAdressPublic(char * IPpublic){
-  	DEBUGLN(("rentre dans set adress ip public"));
-	EthernetClient tcpClient; 									// Client TCP
-	char dnsexitMember[] = "update.dnsexit.com";
-	char hostname[] = "benoit2600.linkpc.net";
-	DNSClient dns1;
-	IPAddress dns_ip(DNS_IP); // OPENDNS
-	IPAddress out_ip;
-	dns1.begin(dns_ip);
-	dns1.getHostByName(dnsexitMember, out_ip);
-	// création chaine getLine:
-	
-	if (tcpClient.connect(out_ip, 80)) {
-		
-		tcpClient.println("GET /RemoteUpdate.sv?login="+ (String) user_dnsexit +"&password="+ (String) password_dnsexit +"&host="+ (String) hostname +"&myip="+ (String) IPpublic +" HTTP/1.1"); // Make a HTTP request:
-		tcpClient.println("Host: update.dnsexit.com");
-		//tcpClient.println("keep-alive");
-		tcpClient.println("Connection: close");
-		tcpClient.println();
-	}
-	else {   
-		// if you didn't get a connection to the server:
-		DEBUGLN(("connection failed"));
-		return 0;
-	}
-    		DEBUGLN(("Réponse de la commande HTTP"));
-
-	while(tcpClient.connected())
-	{
-		if (tcpClient.available())
-		{
-			char c = tcpClient.read();
-			DEBUG((c));
-		}
-		check_button(PINBUTTON1);
-		check_button(PINBUTTON2);
-	}
-	tcpClient.stop();
-	return 1;
-}
-
-/**
- * \brief Va chercher l'adresse P Public du routeur depuis le site checkip.dyndns.com
- * \return retourne l'adresse IP sous la forme : "xxx.xxx.xxx.xxx"
- */
-char * IpAdressPublic(void){
-	EthernetClient tcpClient;	// Client TCP
-
-	boolean ligne = false; // permet de recuperer la ligne contenant le HTML, en ne prenant pas le header
-	char * str = (char* )calloc(sizeof(char),256);
-        if(str == NULL)
-             DEBUGLN(("Allocation de str dans IPAdressPublic impossible"));
-	char * strP = str; // pointeur sur la chaine d'entrée str
-	char DynIP[] = "IP Address: "; //phrase après laquelle ce trouve l'adresse IP"
-	char DynFinIp = '<'; // symbole se trouvant juste après l'adresse IP
-	char * IP = (char*) calloc(15, sizeof(char)); // Adresse IP finale, a placer dans str a la fin de la fonction. (taille max : 15. min : 11)
-         if(IP == NULL)
-             DEBUGLN(("Allocation de IP dans IPAdressPublic impossible"));
-	char * IPPointeur = IP;
-	char dynCheckIp[] = "checkip.dyndns.com";
-	DNSClient dns;
-	IPAddress dns_ip(DNS_IP);
-	IPAddress out_ip;
-	dns.begin(dns_ip);
-	dns.getHostByName(dynCheckIp, out_ip);
-	if (tcpClient.connect(out_ip, 80)) {
-		tcpClient.println("GET / HTTP/1.0"); // Make a HTTP request:
-		tcpClient.println("HOST: "+ (String)dynCheckIp);
-		tcpClient.println("Connection: close");
-		tcpClient.println();
-	}
-	else {
-		// if you didn't get a connection to the server:
-		DEBUGLN(("connection failed"));
-		return NULL;
-	}
-	while(tcpClient.connected())
-	{
-		delay(10);
-		if (tcpClient.available())
-		{
-			char c = tcpClient.read();
-			if((c == '<') || (ligne == true))
-			{
-				ligne = true;
-				//DEBUG((c));
-				*strP = c;
-				strP++;
-			}
-		}
-	}
-	tcpClient.stop();
-	strP = strstr(str, DynIP);
-	strP += (int) strlen(DynIP); // on se place au niveau de l'adresse IP
-	while(*strP != DynFinIp) // On recopie l'adresse IP dans IP
-	{
-		*IPPointeur = *strP;
-		IPPointeur++;
-		strP++;
-	}
-	IPPointeur= '\0';
-	free(str);
-
-	return IP;
-}
 /**
  * \brief Envoi un packet magique a l'adresse passé en paramètre.
  * \param packetBuffer Adresse mac du PC a réveiller.
  */
 void wol_send_packet(byte * packetBuffer){
+	IPAddress broadcastIP(IP_BROADCAST_ARDUINO);					// Adresse IP de Broadcast LAN
+
+	last_debug_msg = "allumage via WOL";
+
+
 	if(packetBuffer == NULL){
  		byte adresseMacWOL[] = {ADRESSE_WOL};
  		byte packetBufferDefaut[MAGIC_PACKET_SIZE];
