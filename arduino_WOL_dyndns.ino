5d4e0c8d59af8f9776b96a75f7c50c43331d28f6
benoit2600 <benoit.fauque@gmail.com>
2017-07-23 10:54:25 +0200

    Utilisation de EtherCard

--- arduino_WOL_dyndns.ino
+++ arduino_WOL_dyndns.ino
@@ -12,28 +12,30 @@
  * (via signal radio 433mHz.
  *
  */
-#include <SPI.h>
-#include <Ethernet.h>
-#include <Dns.h>
-#include <EthernetUdp.h>
+
+#include <EtherCard.h>
+#include <IPAddress.h>
 #include <RCSwitch.h>
 #include "password.h"
+
+
 /* Paramètre utilisateur : */
 
 /* - Paramètre généraux :  */
 #define ADDRESSE_MAC_ARDUINO 	0x00,0xAA,0xBB,0xCC,0xDE,0x03 	// Adresse mac de la arduino
-#define IP_LOCAL_ARDUINO		192,168,0,7			// IP de la arduino sur le réseaux local
+#define IP_LOCAL_ARDUINO		192,168,0,12			// IP de la arduino sur le réseaux local
 #define IP_BROADCAST_ARDUINO	192,168,0,255					// IP de broadcast pour le WOL
 #define MAGIC_PACKET_SIZE 		108								// Taille d'un paquet magique
 #define WOL_PORT				9								// Port de transmission (local) du paquet magique
+#define REQUEST_RATE 5000 // milliseconds
 
 /*	-Parametre bouton*/
 #define PINBUTTON1 6
-#define PINBUTTON2 7
 
 /* Paramètre télécommande, écran*/
 #define PORT_EXTINCTION_SERVER 4243							// Port du serveur d'extinction du PC
 #define PIN_TRANSMITTER	9 									// Pin du transmetteur 433Mhz
+
 /*	- Paramètre de débugage*/
 #define DEBUG 1
 
@@ -46,249 +48,186 @@
 #endif
 
 /*variable global*/
-EthernetUDP udp; 											// Création d'Un objet de classe EthernetUDP
-EthernetServer tcpServer(PORT_EXTINCTION_SERVER);
 String last_debug_msg;
+byte Ethernet::buffer[500];   // a very small tcp/ip buffer is enough here
 
-void setup() {
-	byte arduinoMAC[] = {ADDRESSE_MAC_ARDUINO}; 				// Adresse MAC de la Arduino
-    IPAddress ipLocal(IP_LOCAL_ARDUINO);							// Adresse IP de la Arduino
-	pinMode(PINBUTTON1, INPUT);
-	pinMode(PINBUTTON2, INPUT);
-	//delay(1000);
 
-	#ifdef DEBUG
-		Serial.begin(9600); // set up Serial library at 9600 bps
-	#endif
-	DEBUGLN(("c'est parti !"));
-	Ethernet.begin(arduinoMAC,ipLocal);
-	DEBUGLN(("TCP connecter !"));
-	udp.begin(PORT_DISTANT_WOL);
-	tcpServer.begin();
-	DEBUGLN(("On passe a la boucle principale \n"));
+void setup () {
+  static byte myip[] = {IP_LOCAL_ARDUINO};							// Adresse IP de la Arduino
+  static byte gwip[] = {192, 168, 0, 1};
+  static byte arduinoMAC[] = {ADDRESSE_MAC_ARDUINO};         // Adresse MAC de la Arduino
+
+  pinMode(PINBUTTON1, INPUT);
+
+#ifdef DEBUG
+  Serial.begin(57600); // set up Serial library at 57600 bps
+#endif
+
+  DEBUGLN(("c'est parti !"));
+
+
+  if (ether.begin(sizeof Ethernet::buffer, arduinoMAC) == 0)
+    Serial.println( "Failed to access Ethernet controller");
+
+  ether.staticSetup(myip, gwip);
+
+  while (ether.clientWaitingGw())
+    ether.packetLoop(ether.packetReceive());
+  Serial.println("Gateway found");
+
+  ether.udpServerListenOnPort(&check_wol_magic_packet, PORT_DISTANT_WOL);
+  ether.udpServerListenOnPort(&check_udp_msg, 33663); // check msg extinction
+
+  DEBUGLN(("On passe a la boucle principale \n"));
 }
 
 void loop() {
-    static unsigned char timer =0;
+  ether.packetLoop(ether.packetReceive());
+  delay(100);
+  check_button(PINBUTTON1);
+  //check_ethernet_msg();
 
-	if(timer % 50 == 0){			// On s'occupe du wol reseaux  toutes les 0.5 secondes
-		check_wol_magic_packet();
-		timer = 0;
-	}
-	check_button(PINBUTTON1);
-	//check_button(PINBUTTON2);
-	check_ethernet_msg();
-	delay(10);
-	timer++;
 }
+
+void check_udp_msg(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len) {
+
+  char password[] = "off";
+  boolean passCheck = true;
+  // si on a des données à lire, on va le traiter
+  for ( unsigned char i = 0; i < len; i++ ) {
+    if (data[i] != password[i]) {
+      passCheck = false;
+      break;
+    }
+  }
+  if ( passCheck == true) {
+    DEBUGLN(("extinction des ecrans"));
+    delay(5000);
+    Remote_OFF();  //On eteind les ecrans
+
+  }
+  else {
+    DEBUGLN(("erreur msg udp inconnu"));
+  }
+  while (ether.packetReceive() > 0)
+    DEBUGLN(("on vide la pile de paquets"));
+}
+
+
+
 /**
-* \brief vérifie une connection réseau, et regarde si elle correspond à l'extinction de l'écran.
+   \brief Verifie les paquet magique reçu, et reveil le PC si tout est bon. Une vérification par mot de passe est effectué.
 */
-void check_ethernet_msg(void){
-	EthernetClient client = tcpServer.available();
-	String stringstr;
+boolean wolState = false;
+void check_wol_magic_packet(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len) {
 
-  	if (client) {
-  		DEBUGLN(("y a un client !"));
-  		char c;
-    	if (client.connected()) {
-      		while (client.available()) {
+  char password[] = {PASSWORD_WOL};
+  boolean passCheck = true;
+  // si on a des données à lire, on va le traiter
+  for ( unsigned char i = 102; i < 108; i++ ) {
+    if (data[i] != password[i - 102]) {
+      passCheck = false;
+      break;
+    }
+  }
+  if ( passCheck == true) {
+    DEBUGLN(("envoie packet magique"));
 
-       			c = client.read();
-          		stringstr += c;
-				DEBUGLN((stringstr));
-        	}
-		}
-		stringstr.trim();
-		DEBUG(("msg recu :_"));
-		DEBUGLN((stringstr));
-		test_msg(stringstr, &client);
-		//client.stop();
-	}
+    byte wolMac[] = {ADRESSE_WOL};
+    ether.sendWol(wolMac); // envoi du paquet magique recu.
+    Remote_ON();  //On allume les ecrans
+
+  }
+  else {
+    DEBUGLN(("Mauvais mot de passe "));
+  }
+  while (ether.packetReceive() > 0)
+    DEBUGLN(("on vide la pile de paquets"));
 }
 
-void test_msg(String str, EthernetClient *client){
-	char otherString[50];
-	unsigned long time;
-	time = millis();
 
-	DEBUG(("msg dans test_msg : "));
-	DEBUGLN((str));
+/**
+   \brief Si un bouton est appuyé, on envoie un paquet magique.
+   \param inPin Pin du bouton.
+*/
 
-	if( str == "extinction\n" || str == "extinction"){
-		client->write("ok pour l'extinction\n");
-		delay(5000); //on attend pour verifier la bonne extinction de windows
-		Remote_OFF();
-		last_debug_msg = "Extinction ecran";
-	}
-	if( str == "debug" || str == "debug\n"){
-		last_debug_msg+="\nuptime :";
-		last_debug_msg+=time;
+void check_button(int inPin) {
+  unsigned char  val = digitalRead(inPin);  // read input value
+  if (val == HIGH) {         // check if the input is HIGH (button released)
+    delay(50);
+    val = digitalRead(inPin);
+    if (val == HIGH) {        // check false positive
+      while (val == HIGH) {
+        val = digitalRead(inPin);
+        DEBUGLN(("boucle bouton"));
 
-		last_debug_msg.toCharArray(otherString, last_debug_msg.length() + 1);
-		client->write(otherString);
+        delay(50);
+      }
+      DEBUG(("Bouton pin "));
+      DEBUG((inPin));
+      DEBUGLN((" appuyer"));
+      byte wolMac[] = {ADRESSE_WOL};
+      ether.sendWol(wolMac); // envoi du paquet magique recu.
+      Remote_ON();  //On allume les ecrans
+      DEBUGLN(("fin check bouton"));
+      last_debug_msg = "Bouton appuyé : ";
+      last_debug_msg += inPin ;
 
-		DEBUGLN(("msg de debug sans RaL "));
-		DEBUGLN((otherString));
-		last_debug_msg=""; 	// fix overflow
-	}
-  if(str.substring(0,9) == "ecran_OFF"){
-    unsigned char screen = str.substring(10).toInt();
-    DEBUG(("extinction ecran numero : "));
-
-    DEBUGLN((str.substring(10)));
-    switch_OFF(screen);
-  }
-  if(str.substring(0,8) == "ecran_ON"){
-    unsigned char screen = str.substring(9).toInt();
-    DEBUG(("extinction ecran numero : "));
-
-    DEBUGLN((str.substring(9)));
-    switch_ON(screen);
+    }
   }
 }
-/**
- * \brief Si un bouton est appuyé, on envoie un paquet magique.
- * \param inPin Pin du bouton.
- */
-void check_button(int inPin){
-	unsigned char  val = digitalRead(inPin);  // read input value
-	if (val == HIGH) {         // check if the input is HIGH (button released)
-		delay(100);
-		val = digitalRead(inPin);
-		if (val == HIGH) {        // check false positive
-			while(val == HIGH){
-				val = digitalRead(inPin);
-				DEBUGLN(("boucle bouton"));
-
-				delay(100);
-			}
-			DEBUG(("Bouton pin "));
-			DEBUG((inPin));
-			DEBUGLN((" appuyer"));
-			wol_send_packet(NULL);  // Allume le PC
-			DEBUGLN(("fin check bouton"));
-			last_debug_msg = "Bouton appuyé : ";
-			last_debug_msg += inPin ;
-
-		}
-	}
-}
 
 
-/**
- * \brief Envoi un packet magique a l'adresse passé en paramètre.
- * \param packetBuffer Adresse mac du PC a réveiller.
- */
-void wol_send_packet(byte * packetBuffer){
-	IPAddress broadcastIP(IP_BROADCAST_ARDUINO);					// Adresse IP de Broadcast LAN
-
-	last_debug_msg = "allumage via WOL";
-
-
-	if(packetBuffer == NULL){
- 		byte adresseMacWOL[] = {ADRESSE_WOL};
- 		byte packetBufferDefaut[MAGIC_PACKET_SIZE];
- 		for( char i = 0; i < 6; i++ )
-			packetBufferDefaut[i] = 0xFF;
-
-		for(char i = 6; i < MAGIC_PACKET_SIZE; i++){
-			packetBufferDefaut[i] = adresseMacWOL[i%6];
-		}
-		DEBUGLN(("envoi du paquet magique par defaut."));
-		udp.beginPacket(broadcastIP, WOL_PORT);
-		udp.write(packetBufferDefaut, MAGIC_PACKET_SIZE);
-		udp.endPacket();
-	}
-	else{
-		// envoi du paquet sur l'adresse à l'adresse packetBuffer
-		DEBUGLN(("envoi du paquet magique."));
-		udp.beginPacket(broadcastIP, WOL_PORT);
-		udp.write(packetBuffer, MAGIC_PACKET_SIZE);
-		udp.endPacket();
-	}
-	DEBUGLN(("Allumage ecrans."));
-	Remote_ON();	//On allume les ecrans
-
-}
-/**
- * \brief Verifie les paquet magique reçu, et reveil le PC si tout est bon. Une vérification par mot de passe est effectué.
- */
-void check_wol_magic_packet(void){
-	byte password[] = {PASSWORD_WOL};
-	byte packetBuffer[MAGIC_PACKET_SIZE];
-	boolean passCheck= true;
-	// si on a des données à lire, on va le traiter
-	unsigned char packetSize = udp.parsePacket();
-	if(packetSize)
-	{
-		// lecture des données et stockage dans packetBufffer
-		udp.read(packetBuffer, MAGIC_PACKET_SIZE +6);
-		for( unsigned char i = 102; i < 108; i++ ){
-			if(packetBuffer[i] != password[i-102]){
-				passCheck = false;
-				break;
-			}
-		}
-		if( passCheck== false){
-			DEBUGLN(("Mauvais mot de passe "));
-		}
-		else{
-			wol_send_packet(packetBuffer); // envoi du paquet magique recu.
-		}
-		do{									// On vide la pile de paquet reçu
-			packetSize = udp.parsePacket();
-			DEBUG(("packetSize :  "));
-			DEBUGLN((packetSize));
-			delay(10);
-		}while(packetSize != 0);
-	}
-}
 
 /* Séquence perso d'extinction des écrans + enceinte*/
-void Remote_OFF(void){
-	RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
-	mySwitch.enableTransmit(PIN_TRANSMITTER);
+void Remote_OFF(void) { 
+  RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
 
-	mySwitch.switchOff(2, 3);
-	delay(1500);
-	mySwitch.switchOff(2, 2);
-	delay(1500);
-	mySwitch.switchOff(2, 1);
-	delay(1500);
-  	mySwitch.disableTransmit();
+
+  mySwitch.enableTransmit(PIN_TRANSMITTER);
+  delay(3000);
+  mySwitch.switchOff(2, 3);
+  delay(2000);
+  mySwitch.switchOff(2, 2);
+  delay(2000);
+  mySwitch.switchOff(2, 1);
+  delay(2000);
+  mySwitch.disableTransmit();
 }
 
 /* Extinction d'un interrupteur*/
-void switch_OFF(char num){
-	RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
-	mySwitch.enableTransmit(PIN_TRANSMITTER);
+void switch_OFF(char num) {
+  RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
+  mySwitch.enableTransmit(PIN_TRANSMITTER);
 
-	mySwitch.switchOff(2, num);
-	delay(1500);
-	mySwitch.disableTransmit();
+  mySwitch.switchOff(2, num);
+  delay(2000);
+  mySwitch.disableTransmit();
 }
 
 /*Séquence perso d'allumage des écrans + enceinte*/
-void Remote_ON(void){
-	RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
-	mySwitch.enableTransmit(PIN_TRANSMITTER);
+void Remote_ON(void) {
+  RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
+  mySwitch.enableTransmit(PIN_TRANSMITTER);
 
-  	mySwitch.switchOn(2, 1);
-  	delay(1000);
-  	mySwitch.switchOn(2, 2);
-    delay(1000);
-  	mySwitch.switchOn(2, 3);
-    delay(1000);
-  	mySwitch.disableTransmit();
+  mySwitch.switchOn(2, 1);
+  delay(500);
+  mySwitch.switchOn(2, 2);
+  delay(1000);
+  mySwitch.switchOn(2, 3);
+  delay(1000);
+  mySwitch.disableTransmit();
+
+
+
 }
 
 /*allumage d'un interrupteur*/
-void switch_ON(char num){
-	RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
-	mySwitch.enableTransmit(PIN_TRANSMITTER);
+void switch_ON(char num) {
+  RCSwitch mySwitch = RCSwitch();								// controle l'emetteur 433Mhz
+  mySwitch.enableTransmit(PIN_TRANSMITTER);
 
-  	mySwitch.switchOn(2, num);
-  	delay(1000);
-  	mySwitch.disableTransmit();
+  mySwitch.switchOn(2, num);
+  delay(1000);
+  mySwitch.disableTransmit();
 }
